27c3a428cab91eb6b2ecabd902c6ad15bda69f0b
m-rath <69127760+m-rath@users.noreply.github.com>
2023-05-15 19:36:01 -0400

    reordered - two downloads before two saves

--- get_roberta.py
+++ get_roberta.py
@@ -1,14 +1,16 @@
 from transformers import AutoTokenizer
 from transformers import AutoModelForQuestionAnswering
 
-def run():
-    model_checkpoint = "deepset/roberta-base-squad2-distilled"
 
-    model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
-    model.save_pretrained(model_checkpoint, from_pt=True)
+def run(checkpoint):
+    """ download model and associated tokenizer from https://huggingface.co/models """
 
-    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-    tokenizer.save_pretrained(model_checkpoint, from_pt=True)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    model = AutoModelForQuestionAnswering.from_pretrained(checkpoint)
+
+    tokenizer.save_pretrained(checkpoint, from_pt=True)
+    model.save_pretrained(checkpoint, from_pt=True)
+
 
 if __name__ == "__main__":
-    run()
+    run(checkpoint="deepset/roberta-base-squad2-distilled")
